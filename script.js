237660f9f34fa1cf8c27f2424dd657c913092feb
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-03-31 18:54:52 +0530

    Add add and subtract functions to script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function add() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    let result = a + b;
+
+    document.getElementById("result").innerHTML =
+        "Result = " + result;
+}
+
+function subtract() {
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+
+    let result = a - b;
+
+    document.getElementById("result").innerHTML =
+        "Result = " + result;
+}
